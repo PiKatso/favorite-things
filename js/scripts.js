cd046b7112ec3d81b68b64a3e96c36e248c5b9fe
Dominic Brown and Kat Gifford <kat@>
2017-03-27 09:47:38 -0700

    add javascript functions and calls output

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+  $(".form").submit(function(event){
+    event.preventDefault();
 
+    var favoriteArr = [];
+    console.log("Arr before: " + favoriteArr)
+    favoriteArr.push($("input#input1").val());
+    favoriteArr.push($("input#input2").val());
+    favoriteArr.push($("input#input3").val());
+    favoriteArr.push($("input#input4").val());
+    console.log("Arr after: " + favoriteArr)
+
+    $("#out1").text(favoriteArr[0]);
+    $("#out2").text(favoriteArr[1]);
+    $("#out3").text(favoriteArr[2]);
+    $("#out4").text(favoriteArr[3]);
+
+
+  });
 
 });
